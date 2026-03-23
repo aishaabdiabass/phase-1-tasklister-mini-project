e1ece76c27a8256fcf6cfd6a501728bc1e8b8b8d
aisha abdi <aishaabdi1878@gmail.com>
2026-03-23 16:51:06 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const taskInput = document.querySelector("#new-task-description");
+    const taskText = taskInput.value;
+    
+    if (taskText.trim() !== "") {
+      const taskList = document.querySelector("#tasks");
+      const li = document.createElement("li");
+      li.textContent = taskText;
+      taskList.appendChild(li);
+      
+      taskInput.value = "";
+    }
+  });
 });
